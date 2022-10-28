495de2c76bffcdd98b5d30d527f3a62bfdb7d17e
salbyul <kpast96@gmail.com>
2022-10-28 15:22:00 +0900

    ADD library jjwt SecurityConfig.java

---
+++ src/main/java/com/petmily/petmily/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.petmily.petmily;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf().disable()
+                .formLogin().disable();
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/login", "/logout", "/join").permitAll()
+                .anyRequest().authenticated();
+        return http.build();
+    }
+}
